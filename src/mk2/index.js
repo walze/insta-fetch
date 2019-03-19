1cb6477a3a1b4b39edba0ba2d05a25231f13d5c5
walze <catufuzgu@gmail.com>
2019-03-19 19:32:29 -0300

    updates

--- src/mk2/index.js
+++ src/mk2/index.js
@@ -1,5 +1,5 @@
 
-const axios = require('axios')
+const { default: axios } = require('axios')
 const { JSDOM } = require('jsdom')
 const fs = require('fs')
 const { promisify } = require('util')
@@ -21,10 +21,7 @@ readdir(dir)
 /** @type {string[]} */
 const links = require('./links.json')
 
-const headers = {
-    'Content-Type': 'text/html',
-    'X-Requested-With': 'XMLHttpRequest'
-};
+const headers = { 'Content-Type': 'text/html', 'X-Requested-With': 'XMLHttpRequest' }
 
 const escapeRegExp = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
@@ -32,14 +29,14 @@ const getMetaData = async link => {
     console.log(`Getting metadata ${link}\n`)
 
     const rs = await axios
-        .get(link, { headers })
-        .catch(() => console.log('Error getting link'))
+        .get(link, { headers, timeout: 20000 })
+        .catch(() => console.log(`Error getting ${link}`))
 
-    if (!rs) return console.log('Error getting html')
+    if (!rs) return
 
     const { data: html } = rs
 
-    console.log(`Got metadata ${link}\n`)
+    console.log(`Got metadata ${link}`)
 
     const { window } = new JSDOM(html)
     const { document } = window
@@ -73,24 +70,33 @@ const getMetaData = async link => {
 const getPhoto = async (linkURL, i) => {
     console.log(`Getting photo #${i}`)
 
-    // const photos = await readdir(`${__dirname}/photos/`)
-    const [link, name] = await getMetaData(linkURL)
+    const data = await getMetaData(linkURL)
 
-    if (!name || !link) {
-        console.log('Private profile\n\n')
+    if (!data) {
+        console.log(`Private profile ${linkURL} \n\n`)
         return null
     }
 
-    const rgx = new RegExp(escapeRegExp(name))
-    // const count = photos.filter(a => rgx.test(a)).length
+    const [link, name] = data
 
-    /** @type {string} */
+    if (!name || !link) {
+        console.log(`Private profile ${linkURL} \n\n`)
+        return null
+    }
+
+    /** @type { string } */
     const base64 = await axios
-        .get(link, { responseType: 'arraybuffer' })
-        .then(response => Buffer.from(response.data, 'binary').toString('base64'))
-        .catch(() => console.log('Error parsing base64'))
+        .get(link, { responseType: 'arraybuffer', headers, timeout: 20000 })
+        .then(rs => Buffer.from(rs.data, 'binary').toString('base64'))
+        .catch((err) => console.log('Error parsing base64', err))
 
+    if (!base64) {
+        return null
+    }
 
+    // const photos = await readdir(`${__dirname}/photos/`)
+    // const rgx = new RegExp(escapeRegExp(name))
+    // const count = photos.filter(a => rgx.test(a)).length
     // const filename = `${name}#${count}`
     // const filePath = __dirname + `/photos/${filename}.png`
 
@@ -101,12 +107,67 @@ const getPhoto = async (linkURL, i) => {
     // )
     //     .then(() => console.log(`Written File ${filename}\n\n`))
 
-    return base64
+    return { base64, name }
 }
 
-const files = Promise.all(links.map(getPhoto))
+const wait = t => new Promise(rs => setTimeout(rs, t))
+
+async function map() {
+    const promises = []
+
+    for (var i = 0; i < links.length; i++) {
+        await wait(50);
+        const photo = getPhoto(links[i], i)
+        promises.push(photo)
+    }
+
+    return Promise.all(promises)
+}
+
+const filesPromise = map()
 
 // const syncCall = () => promises.reduce((prev, cur) => prev.then(cur), promises[0]())
 // const asyncCall = () => promises.map(promise => promise())
 
-files.then(console.log)
+// files.then(console.log)
+
+var async = require('async')
+
+filesPromise.then(files => {
+    console.log('Starting to write files')
+
+    const filesFiltered = files.filter(f => !!f)
+
+    async.each(filesFiltered, function ({ name, base64 }, callback) {
+
+        const func = async () => {
+            const photos = await readdir(`${__dirname}/photos/`)
+            const rgx = new RegExp(escapeRegExp(name))
+            const count = photos.filter(a => rgx.test(a)).length
+            const filename = `${name}#${count}`
+            const filePath = __dirname + `/photos/${filename}.png`
+
+            fs.writeFile(
+                filePath,
+                base64,
+                { encoding: 'base64' },
+                callback
+            )
+        }
+
+        func()
+
+    }, function (err) {
+
+        if (err) {
+            // One of the iterations produced an error.
+            // All processing will now stop.
+            console.log('A file failed to process', err)
+        }
+        else {
+            console.log(`\nWritten Files`)
+            console.log('All files have been processed successfully')
+        }
+    })
+
+})
