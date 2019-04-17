d139ea1025eae4ebec89dd6cfdfc655c610f076c
Wallace Fares <wallace.fares@webedia-group.com>
2019-04-17 13:44:37 -0300

    updated write code

--- src/node/write_photos.js
+++ src/node/write_photos.js
@@ -4,12 +4,13 @@ const fs = require('fs')
 const { promisify } = require('util')
 const path = require('path')
 const sizeOf = require('image-size')
+const queue = new (require('promise-queue'))(1, Infinity)
 
 const readdir = promisify(fs.readdir)
 const writeFile = promisify(fs.writeFile)
 const rename = promisify(fs.rename)
 
-/** @type {string[]} */
+/** @type { Array<{url: string, link: string}> } */
 const links = require('../links.json')
 const escapeRegExp = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 const dir = `${__dirname}/photos/`
@@ -41,60 +42,56 @@ const getPhoto = async (photo, i) => {
     }
 }
 
-const map = async (links) => {
-    const promises = []
+/**
+ * @param { Array<{url: string, link: string}> } links
+ */
+const generatePromises = (links) => links.map((link, i) => async () => {
+    const { photo: url, user } = link
+    const photo = await getPhoto(url, i)
 
-    for (var i = 0; i < links.length; i++) {
-        // await wait(1000/30)
-        const { photo: url, user } = links[i]
-        const photo = await getPhoto(url, i)
-        if (!photo) throw new Error('no photo')
+    return { user, photo }
+})
 
-        promises.push({ photo, user })
-    }
 
-    return Promise.all(promises)
+const writePhoto = async (user, photo) => {
+    const photos = await readdir(`${__dirname}/photos/`)
+    const rgx = new RegExp(escapeRegExp(user))
+    const count = photos.filter(a => rgx.test(a)).length
+
+    const filePath = `${__dirname}/photos`
+    const filename = `${user}__(${count})`
+    const ext = 'png'
+    const file1 = `${filePath}/${filename}.${ext}`
+
+    await writeFile(
+        file1,
+        photo,
+        { encoding: 'base64' }
+    )
+
+    const size = await getSize(file1)
+    const { width, height } = size
+
+    const file2 = `${filePath}/${filename}__${width}x${height}.${ext}`
+    await rename(file1, file2)
+
+    return file2
 }
 
+/**
+ * Type casting
+ * @template T
+ * @param { () => T } p
+ * @returns { T }
+ */
+const add2Queue = p => queue.add(p)
 
+generatePromises(links)
+    .map(add2Queue)
+    .map(async dataPromise => {
+        const { user, photo } = await dataPromise
 
-map(links)
-    .then(async files => {
-        console.log(`Starting to write ${files.length} files`)
-
-        for (let i = 0; i < files.length; i++) {
-            const { user, photo } = files[i]
-
-            const func = async () => {
-                const photos = await readdir(`${__dirname}/photos/`)
-                const rgx = new RegExp(escapeRegExp(user))
-                const count = photos.filter(a => rgx.test(a)).length
-
-                const filePath = `${__dirname}/photos`
-                const filename = `${user}__(${count})`
-                const ext = 'png'
-                const file1 = `${filePath}/${filename}.${ext}`
-
-                await writeFile(
-                    file1,
-                    photo,
-                    { encoding: 'base64' }
-                )
-
-                const size = await getSize(file1)
-                const { width, height } = size
-
-                const file2 = `${filePath}/${filename}__${width}x${height}.${ext}`
-                await rename(file1, file2)
-
-                return file2
-            }
-
-            await func()
-                .then(f => console.log(`wrote file ${f}`))
-                .catch(console.error)
-        }
-
-
+        return writePhoto(user, photo)
+            .then(f => console.log(`wrote file ${f}`))
+            .catch(console.error)
     })
-
