3f877d013b2290fc8027f747b5f9c1d004898f3e
Wallace Fares <wallace.fares@webedia-group.com>
2019-04-18 09:11:19 -0300

    fixed responsive

--- src/browser_view/index.js
+++ src/browser_view/index.js
@@ -8,26 +8,24 @@ import { mapObj, shuffle, resolution2Ratio } from './../helpers'
 const $main = document.body
 
 const makeImg = (base64, user = '', { width, height }) => {
-    const container = document.createElement('div')
-    container.classList.add('img')
+    const img = new Image()
+    img.classList.add('lazy')
 
     const [Rx, Ry] = resolution2Ratio(width, height)
 
-    const photoX = (window.innerWidth / 5) - 3
-    const photoY = (Ry * photoX) / Rx
+    const photoX = (window.innerWidth / 5)
 
-    const img = new Image()
+    const x = photoX < 320 ? 320 : photoX
+    const y = (Ry * x) / Rx
 
-    img.style.width = `${photoX}px`
-    img.style.height = `${photoY}px`
+    img.style.width = `${x}px`
+    img.style.height = `${y}px`
     img.style.margin = 0
 
     img.dataset.src = base64
     img.dataset.user = user
     img.alt = user
 
-    img.classList.add('lazy')
-    container.append(img)
 
     return img
 }
