fd031513e8ca9e11d66bf7f76ea46bd3cdb53fda
walze <catufuzgu@gmail.com>
2020-04-04 09:58:56 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+
 import { pipe } from 'ramda'
 import axios from 'axios'
 
-import { of } from 'rxjs'
-import { flatMap, map, tap } from 'rxjs/operators'
+import { of, BehaviorSubject } from 'rxjs'
+import { flatMap, map, tap, toArray, scan, delay } from 'rxjs/operators'
 
-// eslint-disable-next-line no-unused-vars
-import { Request as InstagramRequest } from './Request'
+import { Request as InstagramRequest, Edge, PageInfo } from './Request'
 
 export interface RequestVariables {
   id: string;
@@ -13,29 +15,58 @@ export interface RequestVariables {
   after: string;
 }
 
-const first: RequestVariables = {
+const firstChunk = 'QVFBd2Y3QkppOHFGYWlnZy1HbGNvNVUzU1JESVdSTl9SZ3FZMmM5WUQtSEVLZjViVUg2TFEzRjV2aW1zdXBkMld1QWt1WXd2bnd6T0N4N1JmVXBrOElKQw=='
+
+const buildVariables: (after: string) => RequestVariables = (after) => ({
   id: '890269556',
   first: 42,
-  after: 'QVFEZTd4aGtWMVJhb2k3SWZPaEVrblRVWmZRclBoSUpjS2RfUlcycFhRZDlFSV9tNENxZnFwM3dnUEhxdDRaSmZldmxuVTNoaWhfbFZZOFZPR1JTdHRKcQ=='
-}
+  after
+})
+
+const triple = <a, b, c>(a: a, b: b, c: c) => [a, b, c] as [a, b, c]
 
 const qhash = '8c86fed24fa03a8a2eea2a70a80c7b6b'
 const makeURL = (v: string) => `https://www.instagram.com/graphql/query/?query_hash=${qhash}&variables=${encodeURIComponent(v)}`
 
+const getEdgeData = ({ node: { is_video, shortcode, display_url } }: Edge) => ({
+  is_video,
+  shortcode,
+  display_url
+})
+
 const API = pipe(
+  buildVariables,
   (x: RequestVariables) => JSON.stringify(x),
-  makeURL
+  makeURL,
+  u => of(u)
 )
 
-of(API(first))
+type Edge$ = ReturnType<typeof getEdgeData>
+const edges$ = new BehaviorSubject<Edge$[]>([] as unknown as Edge$[])
+
+edges$
   .pipe(
-    tap(console.warn),
-    flatMap((x: string) => axios
-      .get<InstagramRequest>(x, {
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest'
-        }
-      })),
-    map(x => x.data.data.user.edge_saved_media)
+    scan((acc, one) => [...acc, ...one], [] as Edge$[])
   )
-  .subscribe(console.log)
+  .subscribe(
+    console.warn
+  )
+
+const get = (chunk: string, page = 1) => API(chunk)
+  .pipe(
+    tap(_ => { if (edges$.closed) { throw new Error('done') } }),
+    flatMap((x: string) => axios.get<InstagramRequest>(x)),
+    map(x => x.data.data.user.edge_saved_media),
+    map(x => triple(x.page_info, x.edges.map(getEdgeData), x.count)),
+    tap(([, edges, count]) =>
+      console.log(`Downloading ${count} photos, ${page * edges.length} out of ${count}... | ${((page * edges.length) / count) * 100}%`)
+    ),
+    delay(500)
+  )
+  .subscribe(([info, edges]) => {
+    if (info.has_next_page) { get(info.end_cursor, page + 1) }
+
+    edges$.next(edges)
+  })
+
+get(firstChunk)
